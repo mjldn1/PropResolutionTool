803fd27f78cb9ee424f96655d53f90691e61c35a
mjldn1 <mjalloh1@sheffield.ac.uk>
2023-04-28 19:54:19 +0100

    Added visualisation, trees building but not connected

--- PropResSolver.js
+++ PropResSolver.js
@@ -1,3 +1,5 @@
+
+
 function union(setA, setB) {
   const union = new Set(setA);
 
@@ -9,6 +11,58 @@ function union(setA, setB) {
 }
 
 set = []
+resIteration = 1
+
+
+// create an array with nodes
+var nodes = new vis.DataSet([
+  // {id: 1, label: 'Node 1'},
+  // {id: 2, label: 'Node 2'},
+  // {id: 3, label: 'Node 3'},
+  // {id: 4, label: 'Node 4'},
+  // {id: 5, label: 'Node 5'}
+]);
+
+// create an array with edges
+var edges = new vis.DataSet([
+  // {from: 1, to: 2}
+]);
+
+var edges1 = new vis.DataSet([
+  // {from: 3, to: 2},
+  // {from: 2, to: 4},
+  // {from: 2, to: 5}
+]);
+
+// create a network
+
+var container = document.getElementById('mynetwork');
+console.log(container)
+// provide the data in the vis format
+var data = {
+  nodes: nodes,
+  edges: edges
+};
+
+var data1 = {
+  nodes: nodes,
+  edges: edges1
+};
+var options = {
+  layout: {
+    hierarchical: {
+      direction: 'UD',
+      sortMethod: 'directed'
+    }
+  },
+  physics: {
+    enabled: false
+  }
+
+  
+};
+
+var network = new vis.Network(container, data, options);
 
 //We want to form an array, where each clause is a set
 // var input = "<a,b,¬c><¬a><a,b,c><a,¬b>" {a,b,¬c}{¬a}{a,b,c}{a,¬b}
@@ -67,7 +121,6 @@ function resolve(){
     //takes user input on the propositions to resolve against
     literal = document.getElementById("literal").value;
     literalNeg = "¬".concat(literal)
-    //literalNeg = "¬q"
     console.log(set[clause1])
     console.log(set[clause2])
     console.log(literal)
@@ -81,9 +134,9 @@ function resolve(){
     //   console.log("if statement?")//code successfully resolves.
     // }
 
-    if (set[clause1].has(literal) && set[clause2].has(literalNeg)){
-      set1 = new Set([])
-      set1 = union(set[clause1], ([]))
+    if (set[clause1].has(literal) && set[clause2].has(literalNeg)){//intially code that the new set is just the combination of the 2 clauses
+      set1 = new Set([]) //minus the resolving clauses, however its possible that a literal which is the same as the complement isn't supposed to be removed, and therefore reworked
+      set1 = union(set[clause1], ([])) //to ensure that only the resolving literal are removed
       set1.delete(literal)
       set2 = new Set([])
       set2 = union(set[clause2], ([]))
@@ -91,6 +144,19 @@ function resolve(){
 
       set[set.length] = union(set1, set2)
 
+      string1 = Array.from(set[clause1]).join(',')
+      string2 = Array.from(set[clause2]).join(',')
+      string3 = Array.from(set[set.length-1]).join(',')
+
+      network.body.data.nodes.add([ {id: (3*resIteration)-2, label: `${string1}`}])
+      network.body.data.nodes.add([ {id: (3*resIteration)-1, label: `${string2}`}])
+      network.body.data.nodes.add([ {id: (3*resIteration), label: `${string3}`}])
+
+
+      network.body.data.edges.add([{from: (3*resIteration)-2, to: (3*resIteration)}])
+      network.body.data.edges.add([{from: (3*resIteration)-1, to: (3*resIteration)}])
+
+      resIteration++; 
     }
     else if(set[clause1].has(literalNeg) && set[clause2].has(literal)){
       set1 = new Set([])
@@ -101,6 +167,24 @@ function resolve(){
       set2.delete(literal)
 
       set[set.length] = union(set1, set2)
+
+      // network.body.data.nodes.add([ {id: 1, label: `${set[clause1]}`}]) //able to create tree, however need to fix labels
+      // network.body.data.nodes.add([ {id: 2, label: `${set[clause2]}`}])
+      // network.body.data.nodes.add([ {id: 3, label: `${set[set.length-1]}`}])
+
+      string1 = Array.from(set[clause1]).join(',')
+      string2 = Array.from(set[clause2]).join(',')
+      string3 = Array.from(set[set.length-1]).join(',')
+
+      network.body.data.nodes.add([ {id: (3*resIteration)-2, label: `${string1}`}])
+      network.body.data.nodes.add([ {id: (3*resIteration)-1, label: `${string2}`}])
+      network.body.data.nodes.add([ {id: (3*resIteration), label: `${string3}`}])
+
+
+      network.body.data.edges.add([{from: (3*resIteration)-2, to: (3*resIteration)}])
+      network.body.data.edges.add([{from: (3*resIteration)-1, to: (3*resIteration)}])
+      
+      resIteration++;
     }
 
 
@@ -174,3 +258,24 @@ function resolve(){
 // console.log(item);
 //We will store our propositional variables as an array of sets. The sets allow for the comparison of our clauses
 //and the array will allow us to change the size of our list of conditionals.
+
+
+
+//var network = new vis.Network(container, data1, options);
+
+// network.body.data.edges.add([{from: 1, to: 2}]) //adds edge
+// network.body.data.nodes.add([ {id: 10, label: 'another test'}]) //adds node
+
+
+
+//visualisation ideas
+//visualise after every resolution. May be useful for teaching students, but a may be redundant
+//&decision
+//visualise at end, need to implement a way to keep track of what resolved with what. Tree wont be built for formulaes that are unsatisfiable
+//and thus isnt as useful. Should visualise after resolution
+//goal for today: Implement adding the first 2 nodes.
+
+//every time resolve is pressed, create a new network using the same options and container, but new data.
+//for this new data we can add nodes and edges to the previous iteration of nodes and edges.
+
+//doing some research, its possible to add nodes and edges and there is a function for it
